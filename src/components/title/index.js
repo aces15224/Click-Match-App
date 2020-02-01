9f08b7b60a55e3e2535f50db4402c1f3ac36d50a
Anthony15224 <Anthony15224@hotmail.com>
2020-02-01 10:05:03 -0600

    final

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -8,10 +8,12 @@ import "./title.css"
 function Title(props) {
 
     return  <div id="title" className="col-md-4">
-                <h1 >Click Game</h1>
+              <div id="titleBox">
+                <h1 id="name">Memory Match</h1>
                 <h3 className="score">Your Score: {props.score}</h3>
                 <h3 className="topScore">Top Score: {props.topScore}</h3>
                 <h3 className="message"> {props.message}</h3>
+              </div>
             </div>
           }
 
